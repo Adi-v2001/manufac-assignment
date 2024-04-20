93563d2d8917a5f09282cb830cf2b7e024420c5c
Aditya Verma <adityaverma05092001@gmail.com>
2024-04-20 17:32:05 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ClassStats, addGamma, calculateByClass } from "./Functions";
+import WineData from "./WineData.json";
+
+//Boolean param, to tell function to calculate flavenoid stats or gamma stats
+const flavenoidStats = calculateByClass(WineData, false);
+const gammaAddedData = addGamma(WineData);
+
+const gammaStats = calculateByClass(gammaAddedData, true);
+
+const renderTable = (data: Record<number, ClassStats>) => {
+  return (
+    <table className="min-w-full bg-white border border-gray-200">
+      <thead className="bg-gray-200">
+        <tr>
+          <th className="py-2 px-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">
+            Measure
+          </th>
+          {Object.keys(data).map((className) => (
+            <th
+              key={className}
+              className="py-2 px-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200"
+            >
+              Class {className}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="odd:bg-gray-100">
+          <td className="py-2 px-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Flavonoid Mean</td>
+          {Object.values(data).map((stats, index) => (
+            <td key={index} className="py-2 px-4 border-b border-gray-200">
+              {stats.mean}
+            </td>
+          ))}
+        </tr>
+        <tr className="even:bg-white">
+          <td className="py-2 px-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">
+            Flavonoid Median
+          </td>
+          {Object.values(data).map((stats, index) => (
+            <td key={index} className="py-2 px-4 border-b border-gray-200">
+              {stats.median}
+            </td>
+          ))}
+        </tr>
+        <tr className="odd:bg-gray-100">
+          <td className="py-2 px-4 text-left text-sm font-medium text-gray-700 border-b border-gray-200">Flavonoid Mode</td>
+          {Object.values(data).map((stats, index) => (
+            <td key={index} className="py-2 px-4 border-b border-gray-200">
+              {stats.mode.length === 0 ? 'No Mode' : stats.mode[0]}
+            </td>
+          ))}
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10 space-y-10">
+      <div className="space-y-4">
+        <h1 className="text-xl font-semibold">Flavonoids Statistics</h1>
+        {renderTable(flavenoidStats)}
+      </div>
+      <div className="space-y-4">
+        <h1 className="text-xl font-semibold">Gamma Statistics</h1>
+        {renderTable(gammaStats)}
+      </div>
     </div>
   );
 }
